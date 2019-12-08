38a724193f015bfe9fcf13efdad2c9a570eca7de
1stmuse <olysegs@gmail.com>
2019-12-08 15:47:58 -0500

    started to create the getStarted page

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GetStarted from './getstarted/GetStarted'
+
+function App() {
+  return (
+    <div className="App">
+      <GetStarted/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/getstarted/GetStarted.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+
+class GetStarted extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <h2>Welcome to Player Manager</h2>
+                </div>
+                <div>
+                    <a>Register</a>
+                    <a>Sign in</a>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default GetStarted;
